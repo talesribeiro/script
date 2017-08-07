31d1215df940d253f49d535ff0bcd77ca8264959
talesribeiro <30784178+talesribeiro@users.noreply.github.com>
2017-08-07 08:40:12 -0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -2,15 +2,17 @@
 
 #COMANDOS
 sudo apt-get upgrade && sudo apt-get update && sudo apt-get install
-sudo apt-get install git make screen build-essential autotools-dev autoconf libcurl3 libcurl4-gnutls-dev -y
+#sudo apt-get install git make screen build-essential autotools-dev autoconf libcurl3 libcurl4-gnutls-dev -y
+sudo apt-get install screen build-essential autotools-dev autoconf libcurl3 libcurl4-gnutls-dev -y
 sudo apt install build-essential automake dh-autoreconf libusb-1.0-0-dev cmake g++ -y
 sudo apt-get install libcurl4-openssl-dev libncurses5-dev pkg-config automake yasm -y
 
 # BCC:SUPERNOVA.CC
-wget https://github.com/pooler/cpuminer/releases/download/v2.5.0/pooler-cpuminer-2.5.0.tar.gz
-tar xzf pooler-cpuminer-*.tar.gz
-cd cpuminer-*
+git clone https://github.com/wolf9466/cpuminer-multi
+cd cpuminer-multi/
+chmod +x autogen.sh
+./autogen.sh
 CFLAGS="-march=native" ./configure
 make
-#sudo make install
+sudo make install
 screen minerd -o stratum+tcp://bcc.suprnova.cc:3333 -u talesribeiro.work01 -p x
