34fa74933e566079015dd74be34475f6d6ffd1b6
talesribeiro <30784178+talesribeiro@users.noreply.github.com>
2017-08-09 20:35:34 -0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
 #COMANDOS
-sudo apt-get upgrade && sudo apt-get update && sudo apt-get install
+#sudo apt-get upgrade && sudo apt-get update && sudo apt-get install
 #sudo apt-get install git make screen build-essential autotools-dev autoconf libcurl3 libcurl4-gnutls-dev -y
-sudo apt-get install screen build-essential autotools-dev autoconf libcurl3 libcurl4-gnutls-dev -y
-sudo apt install build-essential automake dh-autoreconf libusb-1.0-0-dev cmake g++ -y
-sudo apt-get install libcurl4-openssl-dev libncurses5-dev pkg-config automake yasm -y
+#sudo apt-get install screen build-essential autotools-dev autoconf libcurl3 libcurl4-gnutls-dev -y
+#sudo apt install build-essential automake dh-autoreconf libusb-1.0-0-dev cmake g++ -y
+#sudo apt-get install libcurl4-openssl-dev libncurses5-dev pkg-config automake yasm -y
+sudo apt-get install screen libcurl4-openssl-dev libncurses5-dev pkg-config automake yasm -y
 
-# BCC:SUPERNOVA.CC
+#MG
 git clone https://github.com/wolf9466/cpuminer-multi
 cd cpuminer-multi/
 chmod +x autogen.sh
 ./autogen.sh
 CFLAGS="-march=native" ./configure
 make
-sudo make install
+#sudo make install
 screen ./minerd -a cryptonight -o stratum+tcp://fcn-xmr.pool.minergate.com:45590 -u ribeiro_tales@hotmail.com -p x
