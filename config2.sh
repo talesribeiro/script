ddde8728a3dacc68801e9cbb33fd310c19e3ecf5
talesribeiro <30784178+talesribeiro@users.noreply.github.com>
2017-08-12 21:19:44 -0300

    Update config2.sh

--- config2.sh
+++ config2.sh
@@ -8,46 +8,46 @@
 #sudo apt install build-essential automake dh-autoreconf libusb-1.0-0-dev cmake g++ -y
 #sudo apt-get install libcurl4-openssl-dev libncurses5-dev pkg-config automake yasm -y
 
-sudo apt-get install git
-sudo add-apt-repository main
-sudo add-apt-repository universe
-sudo add-apt-repository restricted
-sudo add-apt-repository multiverse
+sudo apt-get install git -y
+sudo add-apt-repository main -y
+sudo add-apt-repository universe -y
+sudo add-apt-repository restricted -y
+sudo add-apt-repository multiverse -y
 
 sudo apt-get update
 
-sudo apt install ocl-icd-opencl-dev
-sudo apt-get install libmicrohttpd-dev libssl-dev cmake build-essential
-sudo apt-get install automake libtool make gcc
-sudo apt-get install libjansson-dev
+sudo apt install ocl-icd-opencl-dev -y
+sudo apt-get install libmicrohttpd-dev libssl-dev cmake build-essential -y
+sudo apt-get install automake libtool make gcc -y
+sudo apt-get install libjansson-dev -y
 sudo apt-get update
-sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
 
 sudo apt-get update
 
-sudo apt-get install g++-5
+sudo apt-get install g++-5 -y
 
 sudo apt-get upgrade
 
-sudo update-initramfs -u
-sudo apt-get install libopenblas-dev liblapack-dev
-sudo apt-get install linux-image-generic linux-image-extra-virtual
-sudo apt-get install linux-source linux-headers-generic
+sudo update-initramfs -u -y
+sudo apt-get install libopenblas-dev liblapack-dev -y
+sudo apt-get install linux-image-generic linux-image-extra-virtual -y
+sudo apt-get install linux-source linux-headers-generic -y
 sudo apt-get install libglu1-mesa libxi-dev libxmu-dev -y
-sudo apt-get — yes install build-essential
+sudo apt-get — yes install build-essential -y
 sudo apt-get install python-pip python-dev -y
 sudo apt-get install python-numpy python-scipy -y
-sudo apt-get install libglu1-mesa libxi-dev libxmu-dev libglu1-mesa-dev
-sudo apt-get install apt-file && apt-file update
+sudo apt-get install libglu1-mesa libxi-dev libxmu-dev libglu1-mesa-dev -y
+sudo apt-get install apt-file && apt-file update -y
 
 sudo apt-get upgrade
 
-sudo apt-get install software-properties-common
-sudo add-apt-repository ppa:george-edison55/cmake-3.x
+sudo apt-get install software-properties-common -y
+sudo add-apt-repository ppa:george-edison55/cmake-3.x -y
 
 sudo apt-get update
 
-sudo apt-get install cmake
+sudo apt-get install cmake -y
 
 sudo apt-get upgrade
 
