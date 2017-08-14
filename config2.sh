373bc69273b0ff5e3e93b5e57f43e636e61c6d7b
talesribeiro <30784178+talesribeiro@users.noreply.github.com>
2017-08-13 22:02:14 -0300

    Update config2.sh

--- config2.sh
+++ config2.sh
@@ -9,38 +9,37 @@
 #sudo apt-get install libcurl4-openssl-dev libncurses5-dev pkg-config automake yasm -y
 
 sudo apt-get install git -y
-sudo add-apt-repository main -y
-sudo add-apt-repository universe -y
-sudo add-apt-repository restricted -y
-sudo add-apt-repository multiverse -y
+#sudo add-apt-repository main -y
+#sudo add-apt-repository universe -y
+#sudo add-apt-repository restricted -y
+#sudo add-apt-repository multiverse -y
 
-sudo apt-get update
+sudo apt-get update -y
 
 sudo apt install ocl-icd-opencl-dev -y
 sudo apt-get install libmicrohttpd-dev libssl-dev cmake build-essential -y
 sudo apt-get install automake libtool make gcc -y
 sudo apt-get install libjansson-dev -y
-sudo apt-get update
-sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
 
-sudo apt-get update
+#sudo apt-get update
 
-sudo apt-get install g++-5 -y
 
-sudo apt-get upgrade
+
+
+sudo apt-get upgrade -y
 
 sudo update-initramfs -u -y
 sudo apt-get install libopenblas-dev liblapack-dev -y
 sudo apt-get install linux-image-generic linux-image-extra-virtual -y
 sudo apt-get install linux-source linux-headers-generic -y
 sudo apt-get install libglu1-mesa libxi-dev libxmu-dev -y
-sudo apt-get — yes install build-essential -y
+#sudo apt-get install build-essential -y
 sudo apt-get install python-pip python-dev -y
 sudo apt-get install python-numpy python-scipy -y
 sudo apt-get install libglu1-mesa libxi-dev libxmu-dev libglu1-mesa-dev -y
 sudo apt-get install apt-file && apt-file update -y
 
-sudo apt-get upgrade
+sudo apt-get upgrade -y
 
 sudo apt-get install software-properties-common -y
 sudo add-apt-repository ppa:george-edison55/cmake-3.x -y
@@ -49,11 +48,21 @@ sudo apt-get update
 
 sudo apt-get install cmake -y
 
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+sudo apt-get update
+sudo apt-get install g++-5 -y
+
 sudo apt-get upgrade
+
+sudo apt-get autoremove -y
+
+echo 3 > /proc/sys/vm/drop_caches && sysctl -w vm.drop_caches=3
 
 git clone https://github.com/fireice-uk/xmr-stak-cpu.git
 
 cd xmr-stak-cpu
+sudo chmod 777 && vim donate-level.h
+:%s/constexpr double fDevDonationLevel = 2.0 / 100.0;/constexpr double fDevDonationLevel = 0.0 / 100.0;/g
 cmake -DCMAKE_CXX_COMPILER=/usr/bin/g++-5 -DCMAKE_CC_COMPILER=/usr/bin/gcc5
 make install
 cd bin
